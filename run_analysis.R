df123dab9ccfef8b64441e9fc0e65240a894d032
Chris <mrglass1@users.noreply.github.com>
2020-03-20 01:44:16 +1100

    added task #2 and #3

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,3 @@
-#This R script will generate a tidy data file
-#This is a course project for the Coursera Getting and Cleaning Data
-
 # load dplyr library for easy cleansing
 library(dplyr)
 
@@ -19,3 +16,36 @@ dataFolder <- "UCI HAR Dataset/test"
 subject.test <- read.table(paste(dataFolder,"/subject_test.txt",sep=""),header=FALSE)
 x.test <- read.table(paste(dataFolder,"/X_test.txt",sep=""),header=FALSE)
 y.test <- read.table(paste(dataFolder,"/y_test.txt",sep=""),header=FALSE)
+
+# task #1
+# merge data set to form one data set
+tidydatax <- rbind(x.train,x.test)
+tidydatay <- rbind(y.train,y.test)
+tidydataysubject <- rbind(subject.train,subject.test)
+
+# add headers
+names(tidydatax) <- features$V2
+
+
+# task #2
+# extract the mean and standard deviation for each measurement.
+tidydatax <- tidydatax[, grep("std|mean\\(", names(tidydatax), value=TRUE)] 
+
+# task #3 - Use descriptive activity names to name the activities in the data set
+# this is done by binding new columns from the subject dataset using activity_labels e.g. "3" means "WALKING_DOWNSTAIRS"
+names(tidydatay)="performedactivityID"
+names(activity.labels)=c("performedactivityID","performedactivity")
+
+
+#loop or usr of factors
+tidydatay[,1] <- as.character(factor(tidydatay$performedactivityID, levels = activity.labels[,1], labels = activity.labels[,2]))
+
+#rename the column because it was merged on performedactivityID
+names(tidydatay)="performedactivity" 
+tidydatax <- cbind(tidydatay,tidydatax) 
+#tidyDataX <- cbind(tidyDatay[,2],tidyDataX)
+
+names(tidydataysubject) = "inidividuum"
+tidydatax <- cbind(tidydataysubject,tidydatax) 
+
+
